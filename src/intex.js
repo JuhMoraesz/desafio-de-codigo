5f060e96ae8f81cd807c258cddfc2934b4fc0371
JuhMoraesz <inacio.juliana76@gmail.com>
2024-04-30 13:07:22 -0300

    meu primeiro commit

---
+++ src/intex.js
@@ -0,0 +1,15 @@
+let nomeDoHeroi= " Goku "
+let nivelDoHeroi = 10000
+let nivel ;
+if(nivelDoHeroi < 1000) {nivel = "Ferro"  
+} 
+else if (nivelDoHeroi >= 1001 && nivelDoHeroi <= 2000) { nivel = " Bronze "; }
+else if (nivelDoHeroi >= 2001 && nivelDoHeroi <= 5000) { nivel = " Prata"; }
+else if (nivelDoHeroi >= 5001 && nivelDoHeroi <= 7000) { nivel = " Ouro "; }
+else if (nivelDoHeroi >= 7001 && nivelDoHeroi <= 8000) { nivel = " Platina "; }
+else if (nivelDoHeroi >= 8001 && nivelDoHeroi <= 9000) { nivel = " Ascendente "; }
+else if (nivelDoHeroi >= 9001 && nivelDoHeroi <= 10000) { nivel = " Imortal ";}
+else if (nivelDoHeroi > 10001) { nivel = " radiante "; }
+console.log (" O Herói de nome "
+ + nomeDoHeroi +" está no nível de "+ 
+ nivel)
